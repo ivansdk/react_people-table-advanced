1b257e2b27be3656f5912a84d5d7c7a1581a1ec4
Ivan <ivansdk707@gmail.com>
2023-09-06 20:35:44 +0300

    react_people-table-advanced

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { Person } from '../types';
+import { getPeople } from '../api';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+  const [noMatches, setNoMatches] = useState(false);
+
+  useEffect(() => {
+    setLoader(true);
+    getPeople()
+      .then(setPeople)
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoader(false);
+      });
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
@@ -10,22 +30,36 @@ export const PeoplePage = () => {
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
           <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
+            {!loader && people.length && <PeopleFilters />}
           </div>
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {loader && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {error && !loader && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  Something went wrong
+                </p>
+              )}
 
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
+              {!loader && !error && people.length === 0 && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
-              <p>There are no people matching the current search criteria</p>
+              {!loader && !error && noMatches && (
+                <p>There are no people matching the current search criteria</p>
+              )}
 
-              <PeopleTable />
+              {!loader && !error && people.length > 0 && !noMatches && (
+                <PeopleTable
+                  people={people}
+                  noMatches={noMatches}
+                  setNoMatches={setNoMatches}
+                />
+              )}
             </div>
           </div>
         </div>
